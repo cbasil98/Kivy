6f83ff395179e41c003a9100cca2af1c143a50c4
cbasil98 <cbasil98@gmail.com>
2019-02-16 17:51:42 +0530

    binded button and function
    connection to .kv file required

--- Graph Plotting/Graph.py
+++ Graph Plotting/Graph.py
@@ -1,25 +1,35 @@
-
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.uix.boxlayout import BoxLayout
 import matplotlib.pyplot as plt
+from kivy.uix.button import Button
 import matplotlib.animation as anime
 from matplotlib import style
-
-
 x=[]
 y=[]
 for i in range(10):
-    x.append(0)
-    y.append(0)
+    x.append(i)
+    y.append(i*i)
 style.use('fivethirtyeight')
 fig=plt.figure()
 ax1=fig.add_subplot(1,1,1)
-def animate(i):
-    
-    for j in range(10):
-        x[j]=j
-        y[j]=j*j
-    ax1.clear()
-    ax1.plot(x,y)
-print(x,y) 
-ani=anime.FuncAnimation(fig,animate,interval=1000)
-plt.show()
+class graph(Widget):
+    def plot(self,obj):
+        plt.plot(x,y)
+        plt.show()
+    def __init__(self):
+        super(graph,self).__init__()
+        btn1=Button(text="plot")
+        btn1.bind(on_press=self.plot)
+        self.add_widget(btn1)
+        
+            
+class graphplot(App):
+    def build(self):
+        return graph()
+graphplot().run()
 
+
+
+            
+            
