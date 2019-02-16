9be99561c58a31893601cc3fb38eacaa07e52f62
cbasil98 <cbasil98@gmail.com>
2019-02-16 22:45:47 +0530

    Combined .kv and .py file
    Dropdowmn menu is requires

--- Graph Plotting/Graph.py
+++ Graph Plotting/Graph.py
@@ -10,18 +10,17 @@ y=[]
 for i in range(10):
     x.append(i)
     y.append(i*i)
+
 style.use('fivethirtyeight')
 fig=plt.figure()
 ax1=fig.add_subplot(1,1,1)
-class graph(Widget):
+class graph(BoxLayout):
     def plot(self,obj):
         plt.plot(x,y)
         plt.show()
     def __init__(self):
         super(graph,self).__init__()
-        btn1=Button(text="plot")
-        btn1.bind(on_press=self.plot)
-        self.add_widget(btn1)
+        
         
             
 class graphplot(App):
