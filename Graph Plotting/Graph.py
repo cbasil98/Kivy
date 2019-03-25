359e8009a5ee4f93ec0263593eedcf4341a58195
cbasil98 <cbasil98@gmail.com>
2019-03-25 23:48:24 +0530

    PE project completion

--- Graph Plotting/Graph.py
+++ Graph Plotting/Graph.py
@@ -1,19 +1,24 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.uix.boxlayout import BoxLayout
-import matplotlib.pyplot as plt
 from kivy.uix.button import Button
+from kivy.uix.dropdown import DropDown
+
+import matplotlib.pyplot as plt
 import matplotlib.animation as anime
 from matplotlib import style
+
+
 x=[]
 y=[]
 for i in range(10):
     x.append(i)
     y.append(i*i)
-
 style.use('fivethirtyeight')
 fig=plt.figure()
 ax1=fig.add_subplot(1,1,1)
+
+
 class graph(BoxLayout):
     def plot(self,obj):
         plt.plot(x,y)
@@ -26,6 +31,14 @@ class graph(BoxLayout):
 class graphplot(App):
     def build(self):
         return graph()
+
+
+class CustomDropDown(DropDown):
+    pass
+
+
+
+
 graphplot().run()
 
 
